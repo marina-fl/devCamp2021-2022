1e719c78c439d4b5eca3f0ce1dcee01a43fca28b
Marina Lahniuk <lahniuk.m@gmail.com>
2022-01-08 17:03:15 +0200

    docker-compose

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,17 @@
-const express = require("express");
+const express = require('express');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello API");
+app.get('/', (req, res) => {
+    res.send('Hello API');
 });
 
 app.listen(process.env.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`API started at http://${process.env.HOST}:${process.env.PORT}`);
+        // eslint-disable-next-line no-console
+        console.log(`API started at http://${process.env.HOST}:${process.env.PORT}`);
 });
+
+
+
