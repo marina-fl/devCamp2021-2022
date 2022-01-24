d7989d51307cebfd7d056a7cdd5d0ec27e1d5371
Marina Lahniuk <lahniuk.m@gmail.com>
2022-01-24 02:03:34 +0200

    5

--- front/src/components/body/PostsList.jsx
+++ front/src/components/body/PostsList.jsx
@@ -30,12 +30,7 @@ PostsList.propTypes = {
         id: PropTypes.number.isRequired,
         text: PropTypes.string.isRequired,
         created_at: PropTypes.string.isRequired,
-<<<<<<< HEAD
         iduser: PropTypes.string.isRequired
-=======
-        updated_at: PropTypes.string,
-        user_id: PropTypes.string.isRequired
->>>>>>> d6e8b07... fronttask 5 first try
     })).isRequired
 }
 
