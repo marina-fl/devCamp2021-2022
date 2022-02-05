b582503ff4f81d677b880fbc50475f7fed3552bc
Marina Lahniuk <lahniuk.m@gmail.com>
2022-02-05 23:24:57 +0200

    fixed queries in avatars routes, and it works!

--- api/src/routes/avatars.js
+++ api/src/routes/avatars.js
@@ -7,11 +7,23 @@ const avatarMulter = require("../multer/avatarMulter");
 
 router.post("/:iduser", avatarMulter.single("avatar"), async (req, res) => {
   const { iduser } = req.params;
+  const createdAt = new Date().toLocaleString();
   
   if (req.file) {
     const newPath = req.file.path;
-    await db("avatars").where({ "idusers":iduser }).update({ "link_": newPath });
+    await db.insert({ ...req.body, "idusers": iduser, "link_": newPath, "created_at": createdAt}).into("avatars");
     return res.status(200).json(`File is uploaded to ${newPath}`);
+  }
+  return res.status(500).json("Failed to upload");
+});
+
+router.put("/:iduser", avatarMulter.single("avatar"), async (req, res) => {
+  const { iduser } = req.params;
+  const createdAt = new Date().toLocaleString();
+  if (req.file) {
+    const newPath = req.file.path;
+    await db("avatars").where("idusers", iduser).update({ "link_": newPath, "created_at": createdAt} );
+    return res.status(200).json(`File is uploaded to ${newPath}. Database is updated`);
   }
   return res.status(500).json("Failed to upload");
 });
@@ -19,6 +31,12 @@ router.post("/:iduser", avatarMulter.single("avatar"), async (req, res) => {
 router.get("/", async (req, res) => {
   const avatars = await db.select().from("avatars");
   res.status(200).json(avatars);
+});
+
+router.get("/:iduser", async (req, res) => {
+  const { iduser } = req.params;
+  const avatar = await db.select().from("avatars").where("idusers", iduser);
+  res.status(200).json(avatar);
 });
 
 module.exports = router;
